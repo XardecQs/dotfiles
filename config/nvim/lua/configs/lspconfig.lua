27d5ccd82bdb92fed7db53c8beb8b7df7e0529f7
XardecQs <xavierdelpiero@proton.me>
2025-04-12 22:13:20 -0500

    1.12

--- config/nvim/lua/configs/lspconfig.lua
+++ config/nvim/lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "html", "cssls", "jsonls", "bashls", "pylsp", "texlab" }
+local servers = { "html", "cssls", "jsonls", "bashls", "pylsp", "texlab", "clangd" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
