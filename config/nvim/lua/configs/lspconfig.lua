6f996c9c31d495eb98e0e19da158806db78c60fb
XardecQs <126134158+XardecQs@users.noreply.github.com>
2025-12-12 17:46:02 -0500

    vie 12 dic 2025 17:44

--- config/nvim/lua/configs/lspconfig.lua
+++ config/nvim/lua/configs/lspconfig.lua
@@ -16,6 +16,29 @@ for _, lsp in ipairs(servers) do
   }
 end
 
+local nvim_lsp = require("lspconfig")
+nvim_lsp.nixd.setup({
+   cmd = { "nixd" },
+   settings = {
+      nixd = {
+         nixpkgs = {
+            expr = "import <nixpkgs> { }",
+         },
+         formatting = {
+            command = { "nixfmt" },
+         },
+         options = {
+            nixos = {
+               expr = '(builtins.getFlake ("git+file://" + toString ./.)).nixosConfigurations.k-on.options',
+            },
+            home_manager = {
+               expr = '(builtins.getFlake ("git+file://" + toString ./.)).homeConfigurations."ruixi@k-on".options',
+            },
+         },
+      },
+   },
+})
+
 -- configuring single server, example: typescript
 -- lspconfig.ts_ls.setup {
 --   on_attach = nvlsp.on_attach,
