897ff8f99c57a1ce2ef517d417635ddd789b3c2b
XardecQs <126134158+XardecQs@users.noreply.github.com>
2026-03-28 21:00:37 -0500

    sáb 28 mar 2026 21:00

--- config/nvim/lua/configs/lspconfig.lua
+++ config/nvim/lua/configs/lspconfig.lua
@@ -1,47 +1,50 @@
--- load defaults i.e lua_lsp
-require("nvchad.configs.lspconfig").defaults()
-
-local lspconfig = require "lspconfig"
-
--- EXAMPLE
-local servers = { "html", "cssls", "jsonls", "bashls", "pylsp", "texlab", "clangd", "matlab_ls", "rust_analyzer", "arduino_language_server" }
+-- 1. Obtenemos las utilidades de NvChad (esto sigue siendo útil para mantener la UI)
 local nvlsp = require "nvchad.configs.lspconfig"
+local capabilities = nvlsp.capabilities
+local on_attach = nvlsp.on_attach
 
--- lsps with default config
+-- 2. Servidores con configuración estándar
+local servers = { "html", "cssls", "jsonls", "bashls", "pylsp", "texlab", "clangd", "rust_analyzer" }
+
 for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = nvlsp.on_attach,
-    on_init = nvlsp.on_init,
-    capabilities = nvlsp.capabilities,
-  }
+  -- En la nueva API, se busca configurar mediante el nombre del servidor
+  -- pero por ahora, para mantener compatibilidad con NvChad, lo ideal es:
+  vim.api.nvim_create_autocmd("FileType", {
+    pattern = lsp, -- O el tipo de archivo correspondiente
+    callback = function(args)
+      vim.lsp.start({
+        name = lsp,
+        cmd = { lsp }, -- Esto asume que el binario se llama igual que el lsp
+        capabilities = capabilities,
+        on_attach = on_attach,
+      })
+    end,
+  })
 end
 
-local nvim_lsp = require("lspconfig")
-nvim_lsp.nixd.setup({
-   cmd = { "nixd" },
-   settings = {
-      nixd = {
-         nixpkgs = {
-            expr = "import <nixpkgs> { }",
-         },
-         formatting = {
-            command = { "nixfmt" },
-         },
-         options = {
+--- 3. Configuración específica para nixd (Migración manual)
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "nix",
+  callback = function()
+    vim.lsp.start({
+      name = "nixd",
+      cmd = { "nixd" },
+      settings = {
+        nixd = {
+          nixpkgs = { expr = "import <nixpkgs> { }" },
+          formatting = { command = { "nixfmt" } },
+          options = {
             nixos = {
-               expr = '(builtins.getFlake ("git+file://" + toString ./.)).nixosConfigurations.k-on.options',
+              expr = '(builtins.getFlake ("git+file://" + toString ./.)).nixosConfigurations.k-on.options',
             },
             home_manager = {
-               expr = '(builtins.getFlake ("git+file://" + toString ./.)).homeConfigurations."ruixi@k-on".options',
+              expr = '(builtins.getFlake ("git+file://" + toString ./.)).homeConfigurations."ruixi@k-on".options',
             },
-         },
+          },
+        },
       },
-   },
+      capabilities = capabilities,
+      on_attach = on_attach,
+    })
+  end,
 })
-
--- configuring single server, example: typescript
--- lspconfig.ts_ls.setup {
---   on_attach = nvlsp.on_attach,
---   on_init = nvlsp.on_init,
---   capabilities = nvlsp.capabilities,
--- }
