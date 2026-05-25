dbc244e88a5e9e646f23a932c09920652d252458
XardecQs <126134158+XardecQs@users.noreply.github.com>
2026-05-25 00:46:52 -0500

    fix: corregir paths rotos, eliminar duplicados y plugins tmux congelados

--- config/nvim/lua/configs/lspconfig.lua
+++ config/nvim/lua/configs/lspconfig.lua
@@ -35,10 +35,10 @@ vim.api.nvim_create_autocmd("FileType", {
           formatting = { command = { "nixfmt" } },
           options = {
             nixos = {
-              expr = '(builtins.getFlake ("git+file://" + toString ./.)).nixosConfigurations.k-on.options',
+              expr = '(builtins.getFlake ("git+file://" + toString ./.)).nixosConfigurations.NeoReaper.options',
             },
             home_manager = {
-              expr = '(builtins.getFlake ("git+file://" + toString ./.)).homeConfigurations."ruixi@k-on".options',
+              expr = '(builtins.getFlake ("git+file://" + toString ./.)).nixosConfigurations.NeoReaper.config.home-manager.users.xardec',
             },
           },
         },
